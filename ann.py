9431ed3c44f6d2d3995bdea79fe46b8c63f81c9e
Ubuntu <ubuntu@ip-172-31-35-84.us-east-2.compute.internal>
2021-11-11 02:08:33 +0000

    Submitting ann code

---
+++ ann.py
@@ -0,0 +1,58 @@
+import random
+import string
+class Node():
+    def __init__(self, name):
+        self.children = []
+        self.children_connection_weights = []
+        #for i in range(3): 
+        #self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        self.node_name = name
+
+    def make_children(self, current_layer, nodes_per_layer_map):
+        if current_layer == len(nodes_per_layer_map):
+            return
+        for i in range(nodes_per_layer_map[current_layer]):
+            if (current_layer == 1):
+                self.children.append(Node('Hidden layer node' + str(i + 1)))
+            else:
+                self.children.append(Node('Output layer node' + str(i + 1)))
+        first_born = self.children[0]
+        first_born.make_children(current_layer + 1, nodes_per_layer_map)
+
+        for i in range(1, len(self.children)):
+            self.children[i].children = first_born.children[:]
+
+    def adjust_child_weights(self):
+        if len(self.children) == 0:
+            return
+        self.children_connection_weights = []
+        for i in range(len(self.children)):
+            self.children_connection_weights.append(random.uniform(0, 1))
+            self.children[i].adjust_child_weights()
+
+    def output_children(self, layer):
+        indent = '     ' * layer
+        if len(self.children) == 0:
+            print(f"{indent}{self.node_name}")
+            return
+        print(f"{indent}{self.node_name} is connected to ")
+
+        for i in range(len(self.children)):
+            self.children[i].output_children(layer+1)
+            if i < len(self.children_connection_weights):
+                print(f"{indent}with weight {self.children_connection_weights[i]} ")
+
+master_node = Node('Master node')
+my_first_node = Node('Input layer node1')
+NODE_COUNT_PER_LAYER = [4, 3, 2] 
+my_first_node.make_children(1, NODE_COUNT_PER_LAYER)
+master_node.children.append(my_first_node)
+for i in range(1, len(NODE_COUNT_PER_LAYER)):
+    new_node = Node('Input layer node' + str(i + 1))
+    new_node.children = my_first_node.children[:]
+    master_node.children.append(new_node)
+
+master_node.output_children(0)
+print("!! Set Weights !!")
+master_node.adjust_child_weights()
+master_node.output_children(0)
